b261e40dbda06e2b6b229d4cd992fcd1f0bc0f86
aulisius <aulisius7@gmail.com>
2018-08-18 22:39:48 +0530

    Clean up rollup config

--- index.cjs.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./cjs/snitch.min.js");
-} else {
-  module.exports = require("./cjs/snitch.js");
-}
--- rollup.config.js
+++ rollup.config.js
@@ -1,65 +1,29 @@
 import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
-import copy from "rollup-plugin-copy-assets";
 import filesize from "rollup-plugin-filesize";
-import { uglify } from "rollup-plugin-uglify";
-import { minify } from "uglify-es";
 import pkg from "./package.json";
 
-let INPUT = "src/index.js";
-let peerDependencies = Object.keys(pkg.peerDependencies);
-let createBuild = ({ input, filename, format, external, plugins }) => ({
-  input,
-  external,
+let createBuild = ({ format }) => ({
+  input: pkg.config.source,
+  external: Object.keys(pkg.peerDependencies),
   output: {
-    file: filename,
+    file: `lib/${format}.js`,
     format
   },
-  plugins
-});
-
-let createCjsBuild = ({ production = false }) => {
-  let plugins = [
+  plugins: [
     babel({
       exclude: "node_modules/**"
     }),
     commonjs(),
     filesize()
-  ];
-  if (production) {
-    plugins.push(uglify({ sourceMap: false }, minify));
-  }
-  let filename = `lib/cjs/snitch${production ? ".min" : ""}.js`;
-  return createBuild({
-    input: INPUT,
-    filename,
-    format: "cjs",
-    external: peerDependencies,
-    plugins
-  });
-};
-
-let createESBuild = _ =>
-  createBuild({
-    input: INPUT,
-    filename: "lib/es/snitch.js",
-    format: "es",
-    external: peerDependencies,
-    plugins: [
-      babel({
-        exclude: "node_modules/**"
-      }),
-      commonjs(),
-      copy({
-        assets: ["./index.cjs.js"]
-      }),
-      uglify({ sourceMap: false }, minify),
-      filesize()
-    ]
-  });
+  ]
+});
 
 export default [
-  createCjsBuild({ production: false }),
-  createCjsBuild({ production: true }),
-  createESBuild()
+  createBuild({
+    format: "cjs"
+  }),
+  createBuild({
+    format: "es"
+  })
 ];
